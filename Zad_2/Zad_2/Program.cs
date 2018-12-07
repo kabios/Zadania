5a71f981ef441da84ebdb6214d1f4aa0aa96249a
kaibos <luki867@gmail.com>
2018-12-07 20:21:36 +0100

    Add files via upload

---
+++ Zad_2/Zad_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zad_2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
