06c4e22868e4febd93e6f8bc458667451d38d147
kaibos <luki867@gmail.com>
2018-12-08 19:19:02 +0100

    Klamry w if

--- Zad_1/Zad_1/Program.cs
+++ Zad_1/Zad_1/Program.cs
@@ -6,16 +6,24 @@ namespace Zad_1
     {
         public static void Main(string[] args)
         {
-            for(int i = 1; i <= 100; i++)
+            for (int i = 1; i <= 100; i++)
             {
                 if (i % 3 == 0 && i % 5 == 0)
-                    Console.WriteLine("FizzBuzz");                   
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
                 else if (i % 3 == 0)
+                {
                     Console.WriteLine("Fizz");
+                }
                 else if (i % 5 == 0)
+                {
                     Console.WriteLine("Buzz");
+                }
                 else
+                {
                     Console.WriteLine(i);
+                }
             }
         }
     }
