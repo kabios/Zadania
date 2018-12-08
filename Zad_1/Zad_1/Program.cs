fd13d631022324c15801443765f07c3fb5b26b2c
kaibos <luki867@gmail.com>
2018-12-08 01:38:05 +0100

    Zadanie 1

--- Zad_1/Zad_1/Program.cs
+++ Zad_1/Zad_1/Program.cs
@@ -6,7 +6,17 @@ namespace Zad_1
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("coses");
+            for(int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                    Console.WriteLine("FizzBuzz");                   
+                else if (i % 3 == 0)
+                    Console.WriteLine("Fizz");
+                else if (i % 5 == 0)
+                    Console.WriteLine("Buzz");
+                else
+                    Console.WriteLine(i);
+            }
         }
     }
 }
